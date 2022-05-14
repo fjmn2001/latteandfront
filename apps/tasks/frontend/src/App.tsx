ac4d7d61972ec02a441ed322e1b5f635e539609c
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-05-14 17:40:38 -0400

    feat: adding fetch task repository

--- apps/tasks/frontend/src/App.tsx
+++ apps/tasks/frontend/src/App.tsx
@@ -5,90 +5,45 @@ import TasksHeader from "./components/TasksHeader"
 import TasksBody from "./components/TasksBody"
 import { useEffect, useState } from "react"
 import { Task } from "./types"
-
-
+import {
+  createTask,
+  deleteTask,
+  getAllTasks,
+  updateTask,
+} from "./repositories/fetchTaskRepository"
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true)
   const [tasks, setTasks] = useState<Array<Task>>([])
 
-  useEffect(function() {
-    async function fetchTasks() {
-      const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:FpUXJg0g/todo');
-      const json = await response.json();
-      setTasks(json);
-      setIsLoading(false);
-    }
-    fetchTasks();
-  }, []);
+  useEffect(function () {
+    getAllTasks().then((tasks) => {
+      setTasks(tasks)
+      setIsLoading(false)
+    })
+  }, [])
 
   const handleAddTask = async (task: Task) => {
-    const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:FpUXJg0g/todo', {
-      method: 'POST',
-      body: JSON.stringify({
-        task: task.task,
-        completed: false,
-        important: false
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    });
-    if (response.ok) {
-      async function fetchTasks() {
-        const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:FpUXJg0g/todo');
-        const json = await response.json();
-        setTasks(json);
-        setIsLoading(false);
-      }
-      fetchTasks();
-    } else {
-      const text = await response.text()
-      console.log(text);
+    try {
+      await createTask(task)
+    } catch (e) {
+      console.log(e, "handleAddTask")
     }
   }
 
   const handleToggleTask = async (task: Task) => {
-    const response = await fetch(`https://x8ki-letl-twmt.n7.xano.io/api:FpUXJg0g/todo/${task.id}`, {
-      method: 'POST',
-      body: JSON.stringify({
-        task: task.task,
-        completed: !task.completed,
-        important: true
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    });
-    if (response.ok) {
-      async function fetchTasks() {
-        const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:FpUXJg0g/todo');
-        const json = await response.json();
-        setTasks(json);
-        setIsLoading(false);
-      }
-      fetchTasks();
-    } else {
-      const text = await response.text()
-      console.log(text);
+    try {
+      await updateTask(task)
+    } catch (e) {
+      console.log(e, "handleToggleTask")
     }
   }
 
   const handleRemoveTask = async (taskId: string) => {
-    const response = await fetch(`https://x8ki-letl-twmt.n7.xano.io/api:FpUXJg0g/todo/${taskId}`, {
-      method: 'DELETE'
-    });
-    if (response.ok) {
-      async function fetchTasks() {
-        const response = await fetch('https://x8ki-letl-twmt.n7.xano.io/api:FpUXJg0g/todo');
-        const json = await response.json();
-        setTasks(json);
-        setIsLoading(false);
-      }
-      fetchTasks();
-    } else {
-      const text = await response.text()
-      console.log(text);
+    try {
+      await deleteTask(taskId)
+    } catch (e) {
+      console.log(e, "handleRemoveTask")
     }
   }
 
