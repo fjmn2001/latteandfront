812cb985fa8d1194061eca9330d0e6cea8953a97
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-06-02 06:02:30 -0400

    feat: updating structure. Ref sn

--- apps/books/frontend/src/auth/views/Login.tsx
+++ apps/books/frontend/src/auth/views/Login.tsx
@@ -1,46 +1,75 @@
 import React, { FormEvent, useState } from "react"
+import { JWT_KEY } from "../../shared/const/app"
 
 const Login = () => {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  })
+  const [requestStatus, setRequestStatus] = useState({
+    isLoading: false,
+    hasFailed: false,
+    hasSucceeded: false,
+  })
 
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value)
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((currentForm) => ({
+      ...currentForm,
+      [e.target.name]: e.target.value,
+    }))
   }
 
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value)
-  }
-
-  const handleSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
 
-    fetch("https://librarify.latteandfront.es/api/login_check", {
-      method: "POST",
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username: email, password }),
-    })
-      .then(async (response) => {
-        if (response.ok) {
-          const json = await response.json()
-          console.log(json)
-        } else {
-          console.log(response, "not ok")
-        }
+    try {
+      setRequestStatus({
+        isLoading: true,
+        hasFailed: false,
+        hasSucceeded: false,
       })
-      .catch((error) => console.log(error))
+      const response = await fetch(
+        "https://librarify.latteandfront.es/api/login_check",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: form.email,
+            password: form.password,
+          }),
+        }
+      )
+      if (response.ok) {
+        setRequestStatus({
+          isLoading: false,
+          hasFailed: false,
+          hasSucceeded: true,
+        })
+        const json = await response.json()
+        localStorage.setItem(JWT_KEY, JSON.stringify(json.data))
+      } else {
+        setRequestStatus({
+          isLoading: false,
+          hasFailed: true,
+          hasSucceeded: false,
+        })
+      }
+    } catch (e) {
+      setRequestStatus({
+        isLoading: false,
+        hasFailed: true,
+        hasSucceeded: false,
+      })
+    }
   }
 
   return (
     <div className="row">
       <div className="col">
+        <h2>Login</h2>
         <form onSubmit={handleSubmit}>
-          <div className="alert alert-success" role="alert">
-            A simple success alert—check it out!
-          </div>
           <div className="form-floating mb-3">
             <input
               type="email"
@@ -48,8 +77,9 @@ const Login = () => {
               id="floatingEmail"
               placeholder="name@example.com"
               required={true}
-              value={email}
-              onChange={handleEmailChange}
+              name={"email"}
+              value={form.email}
+              onChange={handleInputChange}
             />
             <label htmlFor="floatingEmail">Email address</label>
           </div>
@@ -60,11 +90,27 @@ const Login = () => {
               id="floatingPassword"
               placeholder="Password"
               required={true}
-              value={password}
-              onChange={handlePasswordChange}
+              name={"password"}
+              value={form.password}
+              onChange={handleInputChange}
             />
             <label htmlFor="floatingPassword">Password</label>
           </div>
+          {requestStatus.isLoading && (
+            <div className="alert alert-info mt-3" role="alert">
+              Loading...
+            </div>
+          )}
+          {requestStatus.hasFailed && (
+            <div className="alert alert-danger mt-3" role="alert">
+              Invalid credentials
+            </div>
+          )}
+          {requestStatus.hasSucceeded && (
+            <div className="alert alert-success mt-3" role="alert">
+              Welcome!
+            </div>
+          )}
           <button type="submit" className="btn btn-primary">
             Submit
           </button>
